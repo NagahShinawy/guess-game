966fc8c7bf1daf2e4313c0dab3774bfe64505fcb
Nagah Shinawy <E-n.Shinawy@lean.sa>
2021-04-25 14:31:02 +0300

    done, failed msg

--- main.py
+++ main.py
@@ -34,17 +34,18 @@ def main():
         remaining = REMAINING_HARD
 
     turns = level.TURNS
-
+    # print(computernumber)
     # ##########################################
 
-    while (guessnumber != computernumber) and (turns >= MIN_REMAINING_TURNS):
-        help_msg(computernumber=computernumber, guessnumber=guessnumber)
+    while (guessnumber != computernumber) and (turns > MIN_REMAINING_TURNS):
         turns -= 1
+        help_msg(computernumber=computernumber, guessnumber=guessnumber)
         print(REMAINING_TURNS_MSG.format(turns=turns))
         number_as_command = CMDNumber()
         guessnumber = GuessNumber(number_as_command.number_value)
 
-    print(DONE.format(turns=remaining - turns))
+    if turns >= 0:
+        print(DONE.format(turns=remaining - turns))
 
     # ##########################################
 
